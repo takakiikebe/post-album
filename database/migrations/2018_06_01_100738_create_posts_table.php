698e770baed198bde9cc2a047b6b43574b9553d8
takaki Ikebe <takakiikebe@me.com>
2020-10-11 11:55:11 +0900

    Add files via upload

---
+++ database/migrations/2018_06_01_100738_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title')->nullable();
+            $table->int('release');
+            $table->text('content')->nullable();
+            $table->string('genre');
+            $table->string('url1');
+            $table->string('url2');
+            $table->int('category_id');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
