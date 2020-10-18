812cce114bfe1574e49f6ce4c133ac37ba2dad9a
takaki Ikebe <takakiikebe@me.com>
2020-10-18 10:55:59 +0900

    Create ListingsController.php

---
+++ app/Http/Controllers/Admin/ListingsController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use App\Jobs\ListingCreated;
+use App\Category;
+use Illuminate\Http\Request;
+
+class ListingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->get('search');
+        $perPage = 25;
+
+        if (!empty($keyword)) {
+            $listings = Listing::where('titleeng', 'LIKE', "%$keyword%")
+                ->orWhere('title', 'LIKE', "%$keyword%")
+                ->orWhere('content', 'LIKE', "%$keyword%")
+                ->latest()->paginate($perPage);
+        } else {
+            $listings = Listing::latest()->paginate($perPage);
+        }
+
+        return view('admin.listings.index', compact('listings'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.listings.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        
+        $requestData = $request->all();
+        
+        Listing::create($requestData);
+
+        dispatch(new ListingCreated);
+
+        return redirect('admin/listings')->with('flash_message', 'Listing added!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $listing = Listing::findOrFail($id);
+
+        return view('admin.listings.show', compact('listing'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $listing = Listing::findOrFail($id);
+
+        return view('admin.listings.edit', compact('listing'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $requestData = $request->all();
+        
+        $listing = Listing::findOrFail($id);
+        $listing->update($requestData);
+
+        return redirect('admin/listings')->with('flash_message', 'Listing updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        Listing::destroy($id);
+
+        return redirect('admin/listings')->with('flash_message', 'Listing deleted!');
+    }
+}
