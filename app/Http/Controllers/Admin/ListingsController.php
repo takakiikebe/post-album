9d5cd2b26261336198ba04bf0ddc747991a3a967
takaki Ikebe <takakiikebe@me.com>
2020-10-18 19:31:03 +0900

    Update ListingsController.php

--- app/Http/Controllers/Admin/ListingsController.php
+++ app/Http/Controllers/Admin/ListingsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use App\Jobs\ListingCreated;
-use App\Category;
+use App\Listing;
 use Illuminate\Http\Request;
 
 class ListingsController extends Controller
